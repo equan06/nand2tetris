2ba638f55afa3be03229fc83d97cf8439ac69c4a
equan06 <31296888+equan06@users.noreply.github.com>
2020-10-31 13:59:06 -0700

    finished proj4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,67 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// There are 256x32 = 8192 consecutive registers. 
+// Bits in each register represent 16 pixels, 1 indicates black, 0 indicates white.
+// Iterate between 16384 and 24575. 24576 stores keyboard input
+
+	@16384// store current location of register 
+	D=A
+	@curr
+	M=D    // init curr to 16384
+	
+	@24576 // Check if any key is pressed (if so, >0, else 0)
+	D=M
+	@LOOPDEL // if 0 then move to delete
+	D;JEQ
+	
+(LOOPINS) // loop over all 256*32=8192 registers
+
+	@24576 // Check if any key is pressed (if so, >0, else 0)
+	D=M
+	@LOOPDEL // if 0 then move to delete
+	D;JEQ
+	
+	
+	@curr
+	D=M
+	@24575
+	D=D-A // D = curr - 24575
+	@LOOPINS
+	D;JGT// break when curr > 24575 or curr - 24575 > 0
+
+	@0  // move to write 1s
+	D=A-1
+	@curr // load location of current register
+	A=M  // switch to that location
+	M=D // Set the current register to -1 = 16 1s
+	@curr
+	M=M+1
+	
+	@LOOPINS
+	0;JMP
+	
+(LOOPDEL)
+	@24576 // Check if any key is pressed (if so, >0, else 0)
+	D=M
+	@LOOPINS // if >0 move to INS
+	D;JGT
+	
+	@curr
+	D=M 
+	@16384
+	D=D-A // curr = curr - 16384
+	@LOOPDEL
+	D;JLT// break when curr < 16384 or curr - 16384 < 0
+
+	@curr
+	A=M 
+	M=0 
+	@curr
+	M=M-1
+
+	@LOOPDEL
+	0;JMP 
+	
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,34 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Put your code here. 
+
+// Idea is to loop R1 times, and each time add R0 to the result to yield the product
+
+	@i    // init i = 0
+	M=0
+	@R2 // init R2 = 0
+	M=0
+	
+(LOOP)
+	@i
+	D=M // set D=i
+	@R1
+	D=M-D // i = i-R1
+	@END
+	D;JLE // if i > R1 goto END // loop: while 0 < R1 - i == exit loop: 0 >= R1 - i
+	@R0  
+	D=M   // set D = R0
+ 	@R2
+	M=M+D // R2 = R2 + R0
+	@i
+	M=M+1 // i = i + 1
+	@LOOP
+	0;JMP // goto LOOP
+(END)
+	@END
+	0;JMP //terminate via inf loop
+	
+	
+	
+	
